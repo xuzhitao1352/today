e42ad695207b86d9a033c145f39c7458518cd146
xuzhitao1352 <xzt_it@163.com>
2019-06-21 15:51:07 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p> Component </p>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  }
+}
+
+</script>
+<style lang="less" scoped>
+</style>
